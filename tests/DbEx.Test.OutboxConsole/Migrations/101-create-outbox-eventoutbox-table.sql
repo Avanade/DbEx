5aaa9753a81f4af5115413a9533948ec02bd996a
Eric Sibly [chullybun] <eric.sibly@avanade.com>
2023-10-17 11:25:02 -0700

    Simply EventOutbox indexing. (#46)

--- tests/DbEx.Test.OutboxConsole/Migrations/101-create-outbox-eventoutbox-table.sql
+++ tests/DbEx.Test.OutboxConsole/Migrations/101-create-outbox-eventoutbox-table.sql
@@ -3,10 +3,10 @@ CREATE TABLE [Outbox].[EventOutbox] (
    * This is automatically generated; any changes will be lost.
    */
 
-  [EventOutboxId] BIGINT IDENTITY (1, 1) NOT NULL PRIMARY KEY NONCLUSTERED ([EventOutboxId] ASC),
+  [EventOutboxId] BIGINT IDENTITY (1, 1) NOT NULL PRIMARY KEY,
   [PartitionKey] NVARCHAR(127) NULL,
   [Destination] NVARCHAR(127) NULL,
   [EnqueuedDate] DATETIME2 NOT NULL,
   [DequeuedDate] DATETIME2 NULL,
-  CONSTRAINT [IX_Outbox_EventOutbox_DequeuedDate] UNIQUE CLUSTERED ([PartitionKey], [Destination], [DequeuedDate], [EventOutboxId])
+  INDEX [IX_Outbox_EventOutbox_DequeuedDate] ([DequeuedDate], [EventOutboxId])
 );
